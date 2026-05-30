62fe26b1a406ce43a771e336093361cae3a9cd1f
Stephen Patane <patane97@gmail.com>
2026-05-31 00:22:03 +1000

    Adding basic card stack component (or more specifically, updating the old unused one)

--- client/src/components/cardStack.tsx
+++ client/src/components/cardStack.tsx
@@ -1,67 +1,83 @@
+import { useMemo } from "react";
 import classNames from "classnames";
-import { ReactElement, useMemo } from "react";
-import { BaseElementProps } from "../types";
-import { Skeleton } from "@heroui/react";
 
-const CardStack = function<T>({
+export default function CardStack<T>({
     cards,
-    children: renderMapFunc,
-    collapsed = false,
+    children: renderCard,
+    selectedIndex = 0,
+    tilt,
     className,
-    classNames: classGroups,
-    style,
-    isLoading,
-    isError,
-    emptyContent = (<span>No cards</span>),
-    errorContent = (<span>An unexpected error has occurred</span>)
-
+    ...props
 }: CardStackProps<T>) {
-    const count = useMemo(() => cards?.length ?? 0, [cards?.length]);
-    const columnsTemplate = useMemo(() => `repeat(${count}, ${collapsed ? "0fr" : "1fr"})`, [collapsed, count]);
-    const rendering = useMemo(() => {
-        if (cards?.length > 0) {
-            return cards.map((card, index) => {
-                const element = renderMapFunc(card);
-                return (
-                    <div
-                        className="relative w-fit h-fit" style={{
-                            gridColumn: `${index + 1} / span ${count - index}`,
-                            gridRow: 1
-                        }}
-                    >
-                        {element}
-                    </div>
-                );
-            });
+    return (
+        <div className={classNames("relative", className)} {...props}>
+            {
+                cards.map((card, index) =>
+                    <StackedCard
+                        key={index}
+                        card={card}
+                        renderCard={renderCard}
+                        selectedIndex={selectedIndex}
+                        index={index}
+                        tilt={tilt}
+                    />
+                )
+            }
+        </div>
+    );
+}
+
+type CardStackProps<T> = Omit<React.HTMLAttributes<HTMLDivElement>, "children"> & {
+    cards: T[];
+    children: (card: T) => React.ReactNode;
+    selectedIndex?: number;
+    tilt?: TiltOptions;
+};
+
+function StackedCard<T>({ card, renderCard, selectedIndex, index, tilt = 0 }: StackedCardProps<T>) {
+    const isTop = index === selectedIndex;
+    const isDismissed = index > selectedIndex;
+    const isBase = index === 0;
+    const depth = selectedIndex - index;
+
+    const cardTilt = useMemo(() => {
+        if (typeof tilt === "number") {
+            return tilt;
         }
-        return <span className="w-full flex flex-col justify-center items-center">{emptyContent}</span>;
-    }, [cards, count, emptyContent, renderMapFunc]);
+        const amount = tilt.amount;
+        if (amount === 0) {
+            return amount;
+        }
+        const variance = tilt.variance ? (Math.random() * 2 - 1) * tilt.variance : 0;
+        const alternate = !tilt.alternate || index % 2 !== 0 ? 1 : -1;
+
+        return (amount + variance) * alternate * depth;
+    }, [depth, index, tilt]);
 
     return (
-        <Skeleton isLoaded={!isLoading} className="rounded-lg h-fit w-full">
-            <div
-                className={classNames("relative grid transition-all duration-700", className, classGroups?.wrapper)}
-                style={{
-                    gridTemplateColumns: columnsTemplate,
-                    ...style
-                }}
-            >
-                {isError ? errorContent : rendering}
-            </div>
-        </Skeleton>
+        <div
+            key={index}
+            className={classNames(
+                "size-full transition-all duration-400 ease-in-out",
+                {
+                    "absolute inset-0": !isBase,
+                    "translate-x-[150%] rotate-10 opacity-0": isDismissed && !isBase,
+                    "brightness-50 -translate-x-2 overflow-hidden": !isDismissed && !isTop
+                }
+            )}
+            style={!isDismissed && !isTop ? { rotate: `${cardTilt}deg` } : undefined}
+        >
+            {renderCard(card)}
+        </div>
     );
-};
+}
 
-type CardStackProps<T> = Omit<BaseElementProps, "children"> & {
-    cards: T[],
-    classNames?: { wrapper?: string, card?: string },
-    children: (card: T) => ReactElement
-    scale?: number,
-    collapsed?: boolean,
-    isLoading?: boolean,
-    isError?: boolean,
-    emptyContent?: ReactElement | string,
-    errorContent?: ReactElement | string
-};
+type StackedCardProps<T> = {
+    card: T;
+    renderCard: (card: T) => React.ReactNode;
+    selectedIndex: number;
+    index: number;
+    tilt?: TiltOptions;
+}
 
-export default CardStack;
+type TiltOptions = number | { amount: number, variance?: number, alternate?: boolean };
